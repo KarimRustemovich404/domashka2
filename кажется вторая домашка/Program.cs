bf312013cf05f818c3e3bd7fa5d54db2831d0077
ASUS ROG <ASUS ROG@DESKTOP-I2BAKAS>
2023-09-07 21:48:42 +0300

    Добавьте файлы проекта.

---
+++ кажется вторая домашка/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace кажется_вторая_домашка
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первую числовую переменную: "); 
+           string number0 = Console.ReadLine();
+            Console.Write("Введите вторую числовую переменную: ");
+            string number1 = Console.ReadLine();
+            Console.WriteLine(number1 + number0);
+            
+
+            Console.ReadKey();
+            
+        }
+    }
+}
